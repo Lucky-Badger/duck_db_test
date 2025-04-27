a95a49f295d55c418e70760f27f950e8caeecdcc
Axel Rodriguez <luckybadger10@gmail.com>
2025-04-26 21:53:38 -0700

    date cast example

--- date_cast.py
+++ date_cast.py
@@ -8,6 +8,7 @@ from duckdb import (
 # Sample DataFrame
 df = pd.DataFrame({
     'a': ['a','b','c','d'],
+    'd': ['a','b','c','d'],
     'date_str': ['20241017', '20231105', '20230220', '20240101'],
     'date_str2': ['20241017', '20231105', '20230220', '20240101']
 })
@@ -16,26 +17,26 @@ df = pd.DataFrame({
 duck_connection = duckdb.connect()
 rel = duck_connection.from_df(df)
 
-# Apply the format function using columns as inputs
-formatted_column1 = duckdb.FunctionExpression(
-    'strptime',
-    col('date_str'),
-    const('%Y%m%d')
-    ).cast('DATE')
+# List of date column names to format
+date_columns = ['date_str', 'date_str2']
+formatted_columns = []
 
-# Select the formatted string
-res = rel.select(f"a, {formatted_column1} AS date")
-res.show()
+# Loop through each column and create the formatted columns
+for i, date_col in enumerate(date_columns, 1):
+    formatted_column = duckdb.FunctionExpression(
+        'strptime',
+        col(date_col),
+        const('%Y%m%d')
+    ).cast('DATE').alias(f'date{i}')  # Aliasing dynamically as date1, date2, etc.
+    
+    # Add the formatted column to the list
+    formatted_columns.append(formatted_column)
 
-
-formatted_column2 = duckdb.FunctionExpression(
-    'strptime',
-    col('date_str2'),
-    const('%Y%m%d')
-).cast('DATE')
-
-# Select the columns including formatted date columns
-res = rel.select(f"a, {formatted_column1} AS date1, {formatted_column2} AS date2")
+print(formatted_columns)
+# Dynamically select columns, including formatted date columns
+unformat_cols = list(set(rel.columns) - set(date_columns))
+columns_to_select = [*unformat_cols] + formatted_columns  # Add 'a' along with formatted columns
+res = rel.select(*columns_to_select)
 
 # Show the result
 res.show()
