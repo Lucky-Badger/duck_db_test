f15fc0e5c6ef96ccd26edcb27191728db67a1557
Axel Rodriguez <luckybadger10@gmail.com>
2025-04-26 21:02:55 -0700

    case_expression example

---
+++ case_expression.py
@@ -0,0 +1,30 @@
+import duckdb
+import pandas as pd
+from duckdb import (
+    ConstantExpression as const,
+    ColumnExpression as col,
+    CaseExpression
+)
+
+df = pd.DataFrame({
+    'a': [1, 2, 3, 4],
+    'b': [True, None, False, True],
+    'c': [42, 21, 13, 14]
+})
+
+# Create a DuckDB relation from the DataFrame
+duck_connection = duckdb.connect()
+
+rel = duck_connection.from_df(df)
+
+hello = const('hello')
+world = const('world')
+
+case = \
+    CaseExpression(condition = col('a') == const(1), value = world)\
+        .when(condition = col('a') == const(2), value = const('test')) \
+        .otherwise(hello)
+
+res = rel.select(f'c,{case} AS case_expression')
+
+print(res)
