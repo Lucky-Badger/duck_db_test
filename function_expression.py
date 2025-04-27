29aa031d58ccc63e707576676c4105c6e8c5ef43
Axel Rodriguez <luckybadger10@gmail.com>
2025-04-26 21:07:44 -0700

    listing functions

---
+++ function_expression.py
@@ -0,0 +1,28 @@
+import duckdb
+import pandas as pd
+from duckdb import (
+    ConstantExpression,
+    ColumnExpression,
+    FunctionExpression
+)
+
+df = pd.DataFrame({
+    'a': [
+        'test',
+        'pest',
+        'text',
+        'rest',
+    ]
+})
+
+# Create a DuckDB relation from the DataFrame
+duck_connection = duckdb.connect()
+
+rel = duck_connection.from_df(df)
+
+ends_with = FunctionExpression('ends_with', ColumnExpression('a'), ConstantExpression('est')).alias('c')
+res = rel.select(ends_with)
+res.show()
+
+# List of functions can be found here
+# https://duckdb.org/docs/stable/sql/functions/overview
